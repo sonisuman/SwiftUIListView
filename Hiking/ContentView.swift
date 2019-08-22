0b62820a78370ae2faa4915625cf743782ff2b5b
Soni suman <soni.suman@impelsys.com>
2019-08-22 11:11:42 +0530

    Initial Commit

---
+++ Hiking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hiking
+//
+//  Created by Soni Suman on 8/22/19.
+//  Copyright © 2019 Soni Suman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
