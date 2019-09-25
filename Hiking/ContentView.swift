1859aecf07bccfc97a8937cbc2aa8403ae2028af
Soni suman <soni.suman@impelsys.com>
2019-09-25 18:10:26 +0530

    add navigation between two view

--- Hiking/ContentView.swift
+++ Hiking/ContentView.swift
@@ -11,24 +11,37 @@ import SwiftUI
 struct ContentView: View {
   var hikes = Hike.All()
   var body: some View {
-    List(hikes) { value in
-      HStack {
-        Image(value.imageURL)
-        .resizable()
-          .frame(width: 100.0,height: 100.0)
-        .cornerRadius(16)
-        VStack(alignment: .leading) {
-          Text(value.name)
-        }
-        VStack(alignment: .leading){
-          Text(String(format: "%.2f",value.miles))
-        }
+    NavigationView {
+      List(hikes) { value in
+        HikeCell(hike: value)
       }
+      .navigationBarTitle(Text("Hikings"))
     }
   }
 }
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
+  }
+}
+  
+struct HikeCell: View {
+  let hike: Hike
+  var body: some View {
+    return NavigationLink(destination: Text(hike.name)) {
+       HStack {
+          Image(hike.imageURL)
+            .resizable()
+            .frame(width: 100.0,height: 100.0)
+            .cornerRadius(16)
+          VStack {
+            Text(hike.name)
+            VStack(alignment: .leading){
+              Text(String(format: "%.2f",hike.miles))
+            }
+          }
+      }
+    }
+ 
   }
 }
